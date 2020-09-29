e02a6c571b689567a19b3341e254f5062e083a3c
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-09-29 10:23:04 -0300

    Basic UI

--- Mars-Map-Router/apCaminhosMarte/Form1.cs
+++ Mars-Map-Router/apCaminhosMarte/Form1.cs
@@ -26,5 +26,82 @@ namespace apCaminhosMarte
         {
             MessageBox.Show("Buscar caminhos entre cidades selecionadas");
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsbOrigem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsbOrigem_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if(e.Index < 0) return;
+            //if the item state is selected them change the back color 
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+            {
+                e = new DrawItemEventArgs(e.Graphics,
+                          e.Font,
+                          e.Bounds,
+                          e.Index,
+                          e.State ^ DrawItemState.Selected,
+                          e.ForeColor,
+                          Color.FromArgb(229, 237, 250)); //Choose the color
+                // Draw the background of the ListBox control for each item.
+                e.DrawBackground();
+                // Draw the current item text
+                e.Graphics.DrawString(lsbOrigem.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds, StringFormat.GenericDefault);
+            }
+            else
+            {
+                // Draw the background of the ListBox control for each item.
+                e.DrawBackground();
+                e.Graphics.DrawString(lsbOrigem.Items[e.Index].ToString(), e.Font, Brushes.WhiteSmoke, e.Bounds, StringFormat.GenericDefault);
+            }
+
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            e.DrawFocusRectangle();
+        }
+
+        private void lsbDestino_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+            //if the item state is selected them change the back color 
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected) { 
+                e = new DrawItemEventArgs(e.Graphics,
+                                          e.Font,
+                                          e.Bounds,
+                                          e.Index,
+                                          e.State ^ DrawItemState.Selected,
+                                          e.ForeColor,
+                                          Color.FromArgb(229, 237, 250)); //Choose the color
+            // Draw the background of the ListBox control for each item.
+            e.DrawBackground();
+            // Draw the current item text
+            e.Graphics.DrawString(lsbOrigem.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds, StringFormat.GenericDefault);
+        }
+        else
+        {
+            // Draw the background of the ListBox control for each item.
+            e.DrawBackground();
+            e.Graphics.DrawString(lsbOrigem.Items[e.Index].ToString(), e.Font, Brushes.WhiteSmoke, e.Bounds, StringFormat.GenericDefault);
+        }
+
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            e.DrawFocusRectangle();
+        }
     }
 }
