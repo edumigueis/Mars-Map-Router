dd018f246fda7818f382e4c916fa46363a73f040
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-05 19:49:22 -0300

    Starting Main Classes

---
+++ Mars-Map-Router/apCaminhosMarte/Data/Cidade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace apCaminhosMarte.Data
+{
+    class Cidade : IComparable<Cidade>
+    {
+        private string nome;
+        private int id, x, y;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+        public Cidade(int id, string nome, int x, int y)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.x = x;
+            this.y = y;
+        }
+
+        public int CompareTo(Cidade other)
+        {
+            return this.id.CompareTo(other.id);
+        }
+    }
+}
---
+++ Mars-Map-Router/apCaminhosMarte/Data/LeitorDeArquivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte.Data
+{
+    class LeitorDeArquivo
+    {
+        private ArvoreBinaria LerArquivoComoArvoreBinaria(string arq)
+        {
+            if (arq.Equals(""))
+                throw new FileNotFoundException("O nome do arquivo não foi fornecido.");
+            if (!Path.GetExtension(arq).Equals(".txt"))
+                throw new ArgumentOutOfRangeException("O arquivo fornecido não é .txt!");
+
+            StreamReader sr = new StreamReader(arq);
+
+            ArvoreBinaria arvore = new ArvoreBinaria();
+            return arvore;
+        }
+
+
+    }
+}
