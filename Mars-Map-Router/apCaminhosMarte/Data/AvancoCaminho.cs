ef94292d2038e1d16bbf60fdf8db1fb93c9caf80
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-10-08 15:31:20 -0300

    Ajustes

---
+++ Mars-Map-Router/apCaminhosMarte/Data/AvancoCaminho.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace apCaminhosMarte.Data
+{
+    class AvancoCaminho : ICloneable
+    {
+        Cidade origem, destino;
+        CaminhoEntreCidades caminho;
+
+        public AvancoCaminho(Cidade origem, Cidade destino, CaminhoEntreCidades cm)
+        {
+            this.origem = origem;
+            this.destino = destino;
+            this.caminho = cm;
+        }
+
+        internal Cidade Origem { get => origem; set => origem = value; }
+        internal Cidade Destino { get => destino; set => destino = value; }
+        internal CaminhoEntreCidades Caminho { get => caminho; set => caminho = value; }
+
+        public object Clone()
+        {
+            AvancoCaminho av = new AvancoCaminho(this.origem, this.destino, this.caminho);
+            return av;
+        }
+    }
+}
---
+++ Mars-Map-Router/apCaminhosMarte/Data/CaminhoEntreCidades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apCaminhosMarte.Data
+{
+    class CaminhoEntreCidades
+    {
+        int dist, tmp, custo;
+
+        public CaminhoEntreCidades(int distancia, int tempo, int custo)
+        {
+            this.dist = distancia;
+            this.tmp = tempo;
+            this.custo = custo;
+        }
+
+        public int Distancia { get => dist; set => dist = value; }
+        public int Tempo { get => tmp; set => tmp = value; }
+        public int Custo { get => custo; set => custo = value; }
+    }
+}
