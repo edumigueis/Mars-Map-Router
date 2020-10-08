99f9bdcc8ff624747f57e33ada6f7b88677db0fb
digo-smithh <smithrodrigues08@gmail.com>
2020-10-08 15:40:28 -0300

    edit

--- Mars-Map-Router/apCaminhosMarte/Data/NoArvore.cs
+++ Mars-Map-Router/apCaminhosMarte/Data/NoArvore.cs
@@ -1,11 +1,10 @@
-﻿
-namespace apCaminhosMarte.Data
+﻿namespace apCaminhosMarte.Data
 {
     class NoArvore<T>
     {
         public T Info { get; set; }
         public NoArvore<T> Esq { get; set; }
-        public NoArvore<T> Dir { get; set; } 
+        public NoArvore<T> Dir { get; set; }
 
         public NoArvore(T info)
         {
